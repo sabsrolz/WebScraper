587ce868fe9f8f6fa86772e9d4a4dd3becbdcf14
sabsrolz <sabsrolz@umich.edu>
2019-11-27 11:28:31 -0600

    created article schema and model

---
+++ models/Article.js
@@ -0,0 +1,30 @@
+let mongoose = require("mongoose");
+
+//reference to Schema constructor
+let Schema = mongoose.Schema;
+
+//create new Schema object for article model
+//comment oject will take id of comment and link it to article
+let ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    comment: {
+        type: Schema.Types.ObjectId,
+        ref: "Comment"
+    }
+});
+
+//create model from article schema that we just created
+let Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
